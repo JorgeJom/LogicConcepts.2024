1847663fff097dd13e7f3184a4263442f37615f7
Jorge <Jorge@DESKTOP-1B7HDOA>
2026-02-15 11:27:04 -0500

    isLeapYear

---
+++ LogicConcepts/leapYear/Program.cs
@@ -0,0 +1,41 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var currentYear = DateTime.Now.Year;
+    var message = string.Empty;
+    var year = ConsoleExtension.GetInt("Ingrese año: ");
+    
+    if (year == currentYear)
+    {
+        message = "es";
+    }
+    else if (year > currentYear)
+    {
+        message = "va a ser";
+    }
+    else
+    {
+        message = "fue";
+    }
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    {
+        Console.WriteLine($"El año [ {year} ], {message} bisiesto");
+    }
+    else
+    {
+        Console.WriteLine($"El año [ {year} ], no {message} bisiesto");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game over.");
