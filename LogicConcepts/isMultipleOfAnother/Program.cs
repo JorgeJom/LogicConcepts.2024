5b4c1f7ee8f921ca47006e7a851c28c016cdbbbb
Jorge <Jorge@DESKTOP-1B7HDOA>
2026-02-15 10:59:28 -0500

    Multiple of another

---
+++ LogicConcepts/isMultipleOfAnother/Program.cs
@@ -0,0 +1,28 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    Console.WriteLine("Ingrese 2 números diferentes");
+    var a = ConsoleExtension.GetInt("Ingrese primer número: ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo número: ");
+
+    if (b % a  == 0)
+    {
+        Console.WriteLine($"[ {a} ] es multiplo de [ {b} ]");
+    }
+    else
+    {
+        Console.WriteLine($"[ {a} ] no es multiplo de [ {b} ]");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game over.");
