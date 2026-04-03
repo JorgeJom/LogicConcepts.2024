2d04e13dd68578d708d65337b9bdee4adc9db494
Jorge <Jorge@DESKTOP-1B7HDOA>
2026-04-03 16:09:17 -0500

    FACTORIAL CALCULUS

---
+++ LogicConcepts/FactorialCalculus/Program.cs
@@ -0,0 +1,24 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("Ingrese número: ");
+    long fact = 1;
+
+    for (int i = 1; i <= n; i++) 
+    {
+        fact *= i;
+    }
+
+    Console.WriteLine($"El factorial es: {fact}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game over.");
